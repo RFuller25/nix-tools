5f6ee8cd68693bca42c1b88d1137ef32923ce651
Rhys Fuller <rfull917@students.bju.edu>
2026-05-06 21:00:34 -0500

    feat(pkg-browser): add first-run setup model and config path persistence

--- tools/pkg-browser/main.go
+++ tools/pkg-browser/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,9 +9,40 @@ import (
 )
 
 func main() {
+	configFlag := flag.String("config", "", "path to NixOS configuration.nix (skips first-run prompt)")
+	flag.Parse()
+
+	var configPath string
+
+	if *configFlag != "" {
+		configPath = *configFlag
+	} else {
+		cfg, err := readAppConfig()
+		if err != nil || cfg.ConfigPath == "" {
+			// First-run setup
+			p := tea.NewProgram(newSetupModel())
+			m, err := p.Run()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			result := m.(setupModel)
+			if result.configPath == "" {
+				os.Exit(0)
+			}
+			if err := writeAppConfig(AppConfig{ConfigPath: result.configPath}); err != nil {
+				fmt.Fprintf(os.Stderr, "warning: could not save config: %v\n", err)
+			}
+			configPath = result.configPath
+		} else {
+			configPath = cfg.ConfigPath
+		}
+	}
+
 	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+	_ = configPath // configPath will be wired into initialModel() in Task 5
 }
